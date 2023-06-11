2f2664214dc031de0978f3793afb8d758f60385f
Jefferson <antjeffersonbatista@gmail.com>
2023-06-11 00:12:28 -0300

    refactor: Change notification response format

--- src/repositories/notification-repository.ts
+++ src/repositories/notification-repository.ts
@@ -8,11 +8,36 @@ const postNotification = async (resultData: resultDataType) => {
 };
 
 const findNotificationsByUser = async (userId: number) => {
-  return await prisma.notifications.findMany({
+  const notifications = await prisma.notifications.findMany({
     where: {
       user_id: userId
+    },
+    select: {
+      id: true,
+      user_id: true,
+      group_id: true,
+      notification_read: true,
+      created_at: true,
+      groups: {
+        select: {
+          group_name: true
+        }
+      }
     }
   });
+
+  const formattedNotifications = notifications.map((notification) => {
+    return {
+      id: notification.id,
+      user_id: notification.user_id,
+      group_id: notification.group_id,
+      group_name: notification.groups.group_name,
+      notification_read: notification.notification_read,
+      created_at: notification.created_at
+    };
+  });
+
+  return formattedNotifications;
 };
 
 const notificationRead = async (userId: number) => {
